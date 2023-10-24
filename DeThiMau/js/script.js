e14ab184e00f2cbb8e8006f7ffeda77962783d73
V <lytranvinh.work@gmail.com>
2023-10-24 08:58:39 +0700

    up js

--- DeThiMau/js/script.js
+++ DeThiMau/js/script.js
@@ -1,2 +1,24 @@
-// coding implementation
-// end coding implementation
+
+function validateForm() {
+    const email = document.getElementById('email').value;
+    const com = document.getElementById('com').value;
+    const rate = document.getElementById('rate').value;
+    if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/i.test(email)) {
+      alert('Please enter a valid email address.');
+      return false;
+    }
+    if (com === '') {
+      alert('Please enter a comment.');
+      return false;
+    }
+    if (isNaN(rate) || rate < 0 || rate > 5) {
+      alert('Please enter a rating between 0 and 5.');
+      return false;
+    }
+    return true;
+  }
+document.getElementById('submit').addEventListener('click', function() {
+    if (!validateForm()) {
+        event.preventDefault();
+    }
+});
